537d4c9853c8e4ce17e2b93ea4eb0e7c40096c89
Mateusz <mateusz.trajdos@gmail.com>
2019-06-17 23:26:44 +0100

    Learning oop php

---
+++ index.php
@@ -0,0 +1,40 @@
+<?php
+
+class Test{ //blueprint, pattern, object factory
+
+
+        //properties = variables;
+        protected $ourProperty = 'I am here';
+
+        //getter
+        function getProperty(){
+          return $this->ourProperty;
+        }
+
+        //setter
+        function setProperty($value){
+          $this->ourProperty = $value;
+        }
+
+        //methods = functions;
+        function returnString(){ // public is by default for methods/functions
+          echo 'from inside object '. $this->ourProperty;
+        }
+
+        function echoOut(){
+            $this->returnString();
+        }
+
+
+}
+
+$object = new Test();
+
+//$object->ourProperty = 'Value changed';
+
+$object->setProperty('I am the new value');
+
+echo $object->getProperty();
+
+
+ ?>
